44f001129ef0758f118f59fdec6f5d0384e1ba8a
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-02-08 10:27:06 -0200

    First commit

---
+++ build/main.js
@@ -0,0 +1,5 @@
+"use strict";
+function main() {
+    console.log("Hi! Soon there will be code.");
+}
+main();
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+function main() {
+  console.log("Hi! Soon there will be code.")
+}
+
+main()
